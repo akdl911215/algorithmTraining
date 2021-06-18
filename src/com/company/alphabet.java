ec5cca6b1e10bbd5bad48c7519635ffe7b8e44d0
LEE Jung Hyun <akdl911215@gmail.com>
2021-06-18 12:37:15 +0900

    1

--- src/com/company/alphabet.java
+++ src/com/company/alphabet.java
@@ -1,20 +1,58 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class alphabet {
     public static void main(String[] args) {
         // 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이
         // 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
 
-        // 알파벳 문장을 입력받는다.
-        // 대문자와 소문자를 똑같이? 같은값으로 취급?
+        Scanner input = new Scanner(System.in);
 
+        int[] arr = new int[26];
+        String s = input.next();
+        // bba를 입력을 입력할 경우
+        System.out.println("s = " + s);
+        System.out.println("");
 
-        // https://st-lab.tistory.com/64
-        int[] arr = new int[26]; // 영문자의 개수는 26개
-        String s = in.next();
-
-        for (int i=0; i < s.length(); i++) {
-
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println("i = " + i); // 0 1 2
+            System.out.println("s.charAt(i) = " + s.charAt(i)); // b b a
+            if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') {
+                arr[s.charAt(i) - 'A']++;
+                System.out.printf("arr[%d] = %s\n\n", i, s.charAt(i) - 'a');
+            }
+            else {
+                arr[s.charAt(i) - 'a']++; // [0 1 0] [0 2 0] [1 2 0]
+                System.out.printf("arr[%d] = %s\n\n", i, s.charAt(i) - 'a');
+            }
+//
         }
+
+        // 예상 출력은
+        // arr[0] = 1 , arr[1] = 1 , arr[2] = 0 이 될것이라고 생각함.
+
+
+        int max = -1;
+        char ch = '?';
+
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println("밑에 i = " + i);
+            System.out.println("max = " + max);
+            System.out.println("arr[i] = " + arr[i]); // [0 2 0]
+            if (arr[i] > max) {
+
+                max = arr[i];
+                System.out.println("if안쪽 max = " + max);
+
+                ch = (char)(i+65);
+                System.out.println("ch = " + ch);
+            }
+            else if (arr[i] == max){
+                ch = '?';
+            }
+        }
+
+        System.out.print("ch : " + ch);
     }
 }
