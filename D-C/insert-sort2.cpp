6c66b7f17ec56a6f016266331f6b0a173ad760cb
akdl911215 <akdl911215@gmail.com>
2022-02-01 22:29:01 +0900

    insert sort

---
+++ D-C/insert-sort2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int n;
+int arr[10];
+
+int main(void) {
+	int i, j, temp, n;
+	scanf("%d", &n);
+	for(i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	for(i = 0; i < n; i++) {
+		j = i;
+		while(j >= 0)
+	}
+}
