0d160c8e1e6e6ef541ac9829eacc38e09a173f2b
akdl911215 <akdl911215@gmail.com>
2022-02-16 18:58:47 +0900

    algorithm

---
+++ Java/src/com/company/sort/arrangeTheNumber_mergeSort.java
@@ -0,0 +1,74 @@
+package com.company.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class arrangeTheNumber_mergeSort {
+    static int N;
+    static int[] sorted, resultArr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        mergeSort(arr, 0, N - 1);
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+    static void mergeSort(int arr[], int m, int n) {
+        if (m < n) {
+            int middle = (m + n) / 2;
+            mergeSort(arr, m, middle);
+            mergeSort(arr, middle + 1, n);
+            merge(arr, m, middle, n);
+        }
+    }
+
+    static void merge(int arr[], int m, int middle, int n) {
+        int i = m;
+        int j = middle + 1;
+        int k = m;
+        sorted = new int[N];
+
+        // 작은 순서대로 배열에 삽입
+        while(i <= middle && j <= n) {
+            if (arr[i] <= arr[j]) {
+                sorted[k] = arr[i];
+                i++;
+            }
+            else {
+                sorted[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        
+        // 남은 데이터도 삽입
+        if (i > middle) {
+            for (int z = j; z <= n; z++) {
+                sorted[k] = arr[z];
+                k++;
+            }
+        }
+        else {
+            for (int z = i; z <= middle; z++) {
+                sorted[k] = arr[z];
+                k++;
+            }
+        }
+        
+        // 정렬된 배열을 삽입
+        for (int z = m; z <= n; z++) {
+            arr[z] = sorted[z];
+        }
+    }
+}
