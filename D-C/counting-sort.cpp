1a547708cc5c29e0f36307f96b87614276f84482
akdl911215 <akdl911215@gmail.com>
2022-02-17 17:36:48 +0900

    algorithm

---
+++ D-C/counting-sort.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void) {
+	int tmp;
+	int count[5];
+	int arr[30] = 
+	{
+		1, 3, 2, 4, 3, 2, 5, 3, 1, 2,
+		3, 4, 4, 3, 5, 1, 2, 3, 5, 2,
+		3, 1, 4, 3, 5, 1, 2, 1, 1, 1
+	};
+	
+	// 모든 count 배열 값 0으로 초기화
+	for (int i = 0; i < 5; i++) {
+		count[i] = 0;
+	} 
+	
+	//
+	for (int i = 0; i < 30; i++) {
+//		printf("i = %d", i);
+
+		count[arr[i]]++;
+		
+//		printf("arr[%d] = %d ", i, arr[i]);
+//		printf("count[%d] ", count[arr[i]]);
+		
+//		printf(" ", count[arr[i]]);
+	} 
+	
+	for (int i = 0; i < 5; i++) {
+		if (count[i] != 0) {
+			for (int j = 0; j < count[i]; j++) {
+				printf("%d ", i);
+			}
+		}
+	}
+}
+
+
