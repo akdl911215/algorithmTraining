d92fd15e596bc0895c59859cba8fa9fa338392a1
akdl911215 <akdl911215@gmail.com>
2022-02-18 23:01:56 +0900

    알고리즘

--- Java/src/com/company/sort/arrangeTheNumber_countingSort4.java
+++ Java/src/com/company/sort/arrangeTheNumber_countingSort4.java
@@ -4,34 +4,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class arrangeTheNumber_countingSort {
+public class arrangeTheNumber_countingSort4 {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int NUMBER_RANGE = Integer.parseInt(br.readLine());
         int N = Integer.parseInt(br.readLine());
 
-        int[] count = new int[NUMBER_RANGE];
+        int[] count = new int[7];
         int[] arr = new int[N];
 
-        // 7개의 배열을 모두 0으로 초기화
-        for (int i = 0; i < NUMBER_RANGE; i++) {
-            count[i] = 0;
-        }
-
-        // N의 크기만큼 숫자 입력
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        // count[0] ~ count[7] 값을 카운팅
+        for (int i = 0; i < 7; i++) {
+            count[i] = 0;
+        }
+
         for (int i = 0; i < N; i++) {
             count[arr[i] - 1]++;
         }
 
-        for (int i = 0; i < NUMBER_RANGE; i++) {
+        for (int i = 0; i < 7; i++) {
             if (count[i] != 0) {
-                for (int j = 0; j < count[i]; j++) {
-                    System.out.print((i + 1) + " ");
+                for (int j = 0; j < count[i]; j++){
+                    System.out.println(i + 1);
                 }
             }
         }
