3cecaf43dc98b42c421c6b88f68801d49c49e259
akdl911215 <akdl911215@gmail.com>
2022-02-21 14:42:50 +0900

    수정중

--- Java/src/com/company/sort/statistics.java
+++ Java/src/com/company/sort/statistics.java
@@ -21,8 +21,13 @@ public class statistics {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        arr = new int[80001];
         mergeArr = new int[N];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+
+        }
 
         // 입력값의 범위 : -4000~ 4000;
         countArr = new int[80001];
@@ -33,6 +38,12 @@ public class statistics {
 
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+            if (min > arr[i]) {
+                min = arr[i];
+            }
         }
 
         // 산술평균 = N개의 수들의 합을 N으로 나눈 값
@@ -41,32 +52,43 @@ public class statistics {
             value += numValue;
         }
         value = value / N;
-//        System.out.println("산술평균 = " + value);
+        System.out.println(value);
 
 
         // 중앙값 = N개의 수들을 증가하는 순서대로 나열했을 경우 그 중앙에 위치하는 값
         mergeSort(arr, 0, N - 1);
         // 중앙은 배열 / 2 중앙 위치치
         int pivot = mergeArr.length / 2;
-//        System.out.println("중앙값 = " + mergeArr[pivot]);
+        System.out.println(mergeArr[pivot]);
 
 
         // 최빈값 = N개의 수들 중 가장 많이 나타나는 값
-        int min = 9999;
+        int mode_max = 0; // 최빈값의 최댓값
+        int mode = 10000;
+        boolean flag = false;
         for (int i = 0; i < N; i++) {
             countArr[arr[i] + FOUR_THOUSAND]++;
         }
-            System.out.println("최빈값 시작");
         for (int i = 0; i < N + FOUR_THOUSAND; i++) {
-            if (countArr[i] != 0) {
-                System.out.println(countArr[i]);
 
-//                if (min > countArr[i]) {
-//                    min = countArr[i];
-//                }
-
+            if (arr[i] != 0) {
+                // 이전 최빈값보다 현재 값의 빈도수가 높을 경우
+                if (mode_max < arr[i]) {
+                    mode_max = arr[i];
+                    mode = i - 4000;
+                    flag = true;
+                }
+                // 이전 최빈값 최댓값과 동일한 경우면서 한 번만 중복되는 경우
+                else if (mode_max == arr[i] && flag == true) {
+                    mode = i - 4000;
+                    flag = false;
+                }
             }
         }
+        System.out.println(mode);
+
+        // 범위 = N개의 수들 중 최댓값과 최솟값의 차이
+        System.out.println(max - min);
     }
 
     static void mergeSort(int arr[], int m, int n) {
