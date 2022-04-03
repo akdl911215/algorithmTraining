1851f7044c1343ea42dee34036674f9930a52024
akdl911215 <akdl911215@gmail.com>
2022-04-03 21:23:08 +0900

    algorithm

--- C/C++/algorithm/Stack/Stack/stack.cpp
+++ C/C++/algorithm/Stack/Stack/stack.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
 
-// push X : 정수 X를 스택에 넣는 연산
-// pop : 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력
-//		 만약 스택에 정수가 없을 경우 -1 출력
-// size : 스택에 들어있는 정수의 개수를 출력
-// empty : 스택이 비어있으면 1, 아니면 0을 출력
-// top : 스택의 가장 위에 있는 정수를 출력한다. 
-//		 스택에 정수가 없는경우 -1출력
+int main(void) {
 
-int N;
-
-int main() {
+	int N;
+	string str;
+	stack<int> stack;
 
 	cin >> N;
 	for (int i = 0; i < N; ++i) {
-
+		cin >> str;
+		if (str == "push") {
+			int num;
+			cin >> num;
+			stack.push(num);
+		} else if (str == "pop") {
+			if (!stack.empty()) {
+				cout << stack.top() << endl;
+				stack.pop();
+			} else {
+				cout << "-1" << endl;
+			}
+		} else if (str == "size") {
+			cout << stack.size() << endl;
+		} else if (str == "empty") {
+			if (str.empty()) {
+				cout << "1" << endl;
+			}
+			else {
+				cout << "0" << endl;
+			}
+ 			
+		} else if (str == "top") {
+			if (!str.empty()) {
+				cout << stack.top() << endl;
+			}
+			else {
+				cout << "-1" << endl;
+			}
+		}
 	}
-
 	return 0;
 }
