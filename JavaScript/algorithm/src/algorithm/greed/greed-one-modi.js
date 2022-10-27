1a1c26f2367bdf07964126853dd164c1ac88ff86
akdl911215 <akdl911215@naver.com>
2022-10-27 17:59:47 +0900

    algorithm greed modi ver2

--- JavaScript/algorithm/src/algorithm/greed/greed-one-modi.js
+++ JavaScript/algorithm/src/algorithm/greed/greed-one-modi.js
@@ -2,13 +2,13 @@ const M = 10; // 배낭의 무게
 const n = 4; // 물건의 개수
 const p = [14, 15, 20, 9]; // 물체 i의 이익
 const w = [4, 3, 5, 3]; // 물체 i의 무게
-const result = []; // 무게 / 이익 으로 나눈 결과 (소수점 포함)
 const ranking = []; // 1kg당 단위 이익이 랭킹 부여
 
-for (let i = 0; i < n; ++i) {
-  result[i] = p[i] / w[i];
-  ranking[i] = 0;
-}
+// 무게 / 이익 으로 나눈 결과 (소수점 포함)
+const result = p.map((el, key) => {
+  ranking[key] = 0;
+  return el / w[key];
+});
 
 for (let i = 0; i < n; ++i) {
   let rank = 1;
@@ -21,11 +21,12 @@ for (let i = 0; i < n; ++i) {
 
 let r = M;
 let totalBenefit = 0;
-for (let i = 0; i < n; ++i) {
+
+ranking.map((el, key) => {
   for (let j = 0; j < n; ++j) {
-    if (i === ranking[j]) {
+    if (key === ranking[j]) {
       if (r === 0) {
-        return;
+        return totalBenefit;
       } else if (w[j] < r) {
         r -= w[j];
         totalBenefit += p[j];
@@ -33,12 +34,10 @@ for (let i = 0; i < n; ++i) {
         let num = p[j] / w[j];
         totalBenefit += r * num;
         r -= r;
-
-        console.log("else totalBenefit : ", totalBenefit);
       }
     }
   }
-}
+});
 
 console.log("totalBenefit : ", totalBenefit);
 // https://akdl911215.tistory.com/410?category=1052678
