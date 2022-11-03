5d3ef2b892ea6c00872ed23a1474df989ecccbe0
akdl911215 <akdl911215@naver.com>
2022-11-03 16:34:25 +0900

    improvment work

--- JavaScript/algorithm/src/algorithm/a-test/two.js
+++ JavaScript/algorithm/src/algorithm/a-test/two.js
@@ -3,16 +3,12 @@ const b = 5000;
 const budget = 23000;
 // 3000, 5000, 23000;
 function solution(a, b, budget) {
-  console.log(typeof a);
-  console.log(typeof b);
-  console.log(typeof budget);
   let answer = 0;
 
   for (let i = 0; i * a <= budget; ++i) {
     for (let j = 0; j * b <= budget - a * i; ++j) {
       const resultA = i * a;
       const resultB = j * b;
-      console.log(`resultA : ${resultA} , resultB : ${resultB}`);
       if (budget - (Number(resultA) + Number(resultB)) === 0) {
         answer++;
       }
