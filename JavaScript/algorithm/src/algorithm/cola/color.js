9b2ec7e9e79be918b9e75c9c4247db702e202d3f
akdl911215 <akdl911215@naver.com>
2022-11-10 15:54:39 +0900

    cola algorithm

--- JavaScript/algorithm/src/algorithm/cola/color.js
+++ JavaScript/algorithm/src/algorithm/cola/color.js
@@ -1,16 +1,31 @@
-let check = 0;
 let check2 = 0;
+let check3 = 0;
 
 function solution(a, b, n) {
   let answer = 0;
   console.log(`a: ${a}, b: ${b}, n: ${n}`);
 
-  for (let i = 0; i < n; i += a) {
-    console.log("?");
-    check = check + b;
+  if (n === 1 && check3 > 0) {
+    n += check3;
+    check3--;
   }
-  console.log("check : ", check);
-  // n / a
+
+  if (n === 0) {
+    answer = check2;
+    console.log("n === 0 answer : ", answer);
+    return answer;
+  }
+  let check = 0;
+
+  const num = n / a;
+  check += num;
+  check2 += num;
+  const num2 = n % a;
+  if (num2 > 0) {
+    check3 += num2;
+  }
+
+  solution(a, b, Math.floor(check));
 
   return answer;
 }
