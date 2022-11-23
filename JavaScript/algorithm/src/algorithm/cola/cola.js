78959295fdc669261d21a59a2dcfb8de3abb8c93
akdl911215 <akdl911215@naver.com>
2022-11-23 17:56:19 +0900

    algorithm

--- JavaScript/algorithm/src/algorithm/cola/cola.js
+++ JavaScript/algorithm/src/algorithm/cola/cola.js
@@ -1,17 +1,11 @@
 function solution(a, b, n) {
   let answer = 0;
-
-  let num = 0;
-  let num2 = 0;
-
-  for (let i = 0; i < n; i += a) {
-    num = n / a;
-    num2 = n % a;
-    num = num + num2;
+  while (n >= a) {
+    let num = Math.floor(n / a)
+    let num2 = n % a;
+    n = num2 + num
+    answer = answer + (num * b)
   }
-
-  console.log("num : ", num);
-  console.log("num2 : ", num2);
 
   return answer;
 }
