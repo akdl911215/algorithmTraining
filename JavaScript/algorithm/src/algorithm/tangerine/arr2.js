d47ac24495d0b3db4afa5be4b303b3ba42fd5d49
akdl911215 <akdl911215@naver.com>
2022-11-28 19:46:23 +0900

    tangerine std

--- JavaScript/algorithm/src/algorithm/tangerine/arr2.js
+++ JavaScript/algorithm/src/algorithm/tangerine/arr2.js
@@ -1,5 +1,5 @@
 // const arr = [1, 1, 2, 2, 3]
-const arr = ['a', 'a', 'b', 'b', 'c']
+const arr = ['f', 'a', 'b', 'b', 'c']
 
 const arr2 = arr.filter((item, index) => arr.indexOf(item) !== index)
 
--- JavaScript/algorithm/src/algorithm/tangerine/tangerine.js
+++ JavaScript/algorithm/src/algorithm/tangerine/tangerine.js
@@ -1,23 +1,34 @@
 function solution(k, tangerine) {
-    console.log(`k: ${k}, tangerine: ${tangerine}`)
-    let answer = 0;
+    const arr = tangerine.filter((item, index) => tangerine.indexOf(item) !== index);
+    console.log('arr : ', arr)
 
-    while (k > 0) {
-        //
+    // 중복인게 3개
+    // 과일 수 6개
+    // 중복 x 2 가 과일 숫자보다 작으면
+    let answer = 0;
+    if (arr.length * 2 >= k) {
+        answer = arr.length
     }
-    return answer;
+    else if (arr.length * 2 < k) {
+        answer = arr.length
+    }
+    else if (arr.length * 2 > k) {
+        answer = Math.floor(k / 2)
+    }
+
+    return answer
 }
 
-const k = 6
-const tangerine = [1,3,2,5,4,5,2,3]
+// const k = 6
+// const tangerine = [1,3,2,5,4,5,2,3]
 // result : 3
 
-// const k = 4
-// tangerine = [1,3,2,5,4,5,2,3]
+const k = 4
+tangerine = [1,3,2,5,4,5,2,3]
 // result : 2
 
 // const k = 2
 // tangerine = [1,1,1,1,2,2,2,3]
 // result : 1
 
-solution(k, tangerine)
+console.log(solution(k, tangerine))
