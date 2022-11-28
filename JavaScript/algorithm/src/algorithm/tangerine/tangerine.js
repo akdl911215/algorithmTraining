633bdc108e92419d770009d85d028c3ab6072764
akdl911215 <akdl911215@naver.com>
2022-11-28 21:51:55 +0900

    tangerine :

--- JavaScript/algorithm/src/algorithm/tangerine/tangerine.js
+++ JavaScript/algorithm/src/algorithm/tangerine/tangerine.js
@@ -1,30 +1,26 @@
 function solution(k, tangerine) {
-    const arr = tangerine.filter((item, index) => tangerine.indexOf(item) !== index);
-    console.log('arr : ', arr)
-
-    // 중복인게 3개
-    // 과일 수 6개
-    // 중복 x 2 가 과일 숫자보다 작으면
     let answer = 0;
-    if (arr.length * 2 >= k) {
-        answer = arr.length
-    }
-    else if (arr.length * 2 < k) {
-        answer = arr.length
-    }
-    else if (arr.length * 2 > k) {
-        answer = Math.floor(k / 2)
+    const arr = tangerine.filter((item, index) => tangerine.indexOf(item) !== index);
+    const divisor = Math.floor(k / 2);
+
+    if (arr.length > divisor) {
+        answer = divisor;
+    } else {
+        answer = arr.length;
     }
 
     return answer
 }
 
+const k = 3
+const tangerine = [1, 1, 2, 2, 3, 3, 4, 4, 5]
+
 // const k = 6
 // const tangerine = [1,3,2,5,4,5,2,3]
 // result : 3
 
-const k = 4
-tangerine = [1,3,2,5,4,5,2,3]
+// const k = 4
+// tangerine = [1,3,2,5,4,5,2,3]
 // result : 2
 
 // const k = 2
