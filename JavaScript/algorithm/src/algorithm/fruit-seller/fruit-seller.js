f45d88fe495b8b38905194be12db1578ebd1c6fb
akdl911215 <akdl911215@naver.com>
2022-12-18 19:56:41 +0900

    fruit algorithm

--- JavaScript/algorithm/src/algorithm/fruit-seller/fruit-seller.js
+++ JavaScript/algorithm/src/algorithm/fruit-seller/fruit-seller.js
@@ -1,38 +1,36 @@
 function solution(k, m, score) {
     // 사과의 최대 점수 k, 한 상자에 들어가는 사과의 수 m, 사과들의 점수 score
     let answer = 0;
+    const forLength = score.length;
+    console.log('forLength : ', forLength)
 
     const sortArr = score.sort((a,b) => b - a);
     console.log("sortArr : ",sortArr);
 
     let box = [];
-    for (let i = 0; i < sortArr.length; i += m) {
+    let count = forLength;
+    while(count >= m) {
         box = sortArr.splice(0, m);
-        console.log('box : ', box)
-
+        console.log('box : ', box);
         const minScore = box.map(el => el).pop();
-        console.log('minScore : ', minScore)
-        const boxCount = Math.floor(sortArr.length / m)
-        console.log('boxCount : ', boxCount)
+        console.log('micScore : ', minScore);
 
-        // (최저 사과 점수) x (한 상자에 담긴 사과 개수) x (상자의 개수) = 2 x 4 x 1 = 8
-        // const minScoree = sortArr.pop()
-
-        answer = minScore * m * boxCount
+        answer += minScore * m * 1
+        count -= m;
     }
 
     console.log("answer :: " ,answer)
     return answer;
 }
 
-// const k = 3;
-// const m = 4;
-// const score = [1, 2, 3, 1, 2, 3, 1];
+const k = 3;
+const m = 4;
+const score = [1, 2, 3, 1, 2, 3, 1];
 // result : 8
 
-const k = 4;
-const m = 3;
-const score = [4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2];
+// const k = 4;
+// const m = 3;
+// const score = [4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2];
 // result : 33
 
 console.log(solution(k,m,score))
