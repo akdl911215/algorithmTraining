ea121e1af012681c9727f20014680ebe6449827e
akdl911215 <akdl911215@naver.com>
2022-12-28 21:42:53 +0900

    defense game al modi

--- JavaScript/algorithm/src/algorithm/defense-game/defense-game.js
+++ JavaScript/algorithm/src/algorithm/defense-game/defense-game.js
@@ -3,38 +3,46 @@ function solution(n, k, enemy) {
     let answer = k;
 
     enemy.sort((a, b) => b - a).splice(0, k);
-    if(enemy.length === 0) return answer;
+        // .splice(0, k);
+    // if(enemy.length === 0) return answer;
     console.log('enemy : ', enemy) // 5, 10, 30 제외 출력
 
     // k 보다 작거나 같은거 배열에 담고 제일 큰 수를 반환 전략으로 변경
     let checkingBox = [];
-    for (let i = 0; i < enemy.length - 1; i++) {
-        let count = enemy[i];
-        answer++;
-        for (let j = i + 1; j < enemy.length; j++) {
-            count += enemy[j];
-            answer++;
-            if (count <= k) {
-                console.log('!!')
-                checkingBox.push(answer)
-            }
+    // for (let i = 0; i < enemy.length - 1; i++) {
+    //     let count = enemy[i];
+    //     answer++;
+    //     for (let j = i + 1; j < enemy.length; j++) {
+    //         count += enemy[j];
+    //         answer++;
+    //         if (count <= k) {
+    //             console.log('!!')
+    //             checkingBox.push(answer)
+    //         }
+    //     }
+    //     answer = k;
+    // }
+
+    for (let i = enemy.length - 1; i >= 0; i--) {
+        if (answer >= 7) {
+         break
         }
-        answer = k;
+        console.log('enemy[i] : ', enemy[i])
+        answer += enemy[i]
     }
 
-    console.log('checking : ', checkingBox)
-
-    return checkingBox.sort((a, b) => a + b).pop();
+    console.log(answer)
+    return answer;
 }
-
-const n = 7;
-const k = 3;
-const enemy = [4, 2, 4, 5, 3, 3, 1, 10, 30, 1 , 2];
-// result:
 
 // const n = 7;
 // const k = 3;
-// const enemy = [4, 2, 4, 5, 3, 3, 1];
+// const enemy = [4, 2, 4, 5, 3, 3, 1, 10, 30, 1 , 2];
+// result:
+
+const n = 7;
+const k = 3;
+const enemy = [4, 2, 4, 5, 3, 3, 1];
 // result: 5
 
 // const n = 2;
