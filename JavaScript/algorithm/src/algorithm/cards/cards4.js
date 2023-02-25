d0de21285a326164aa4a68f344824c3cf7cd4d66
akdl911215 <akdl911215@naver.com>
2023-02-25 21:25:07 +0900

    cards study

--- JavaScript/algorithm/src/algorithm/cards/cards4.js
+++ JavaScript/algorithm/src/algorithm/cards/cards4.js
@@ -1,28 +1,15 @@
 function solution(cards1, cards2, goal) {
-    let answer = "No";
 
-    let count = 0;
-    for (let i = 0; i < goal.length; ++i) {
-        count++;
-
-        if (goal[i] === cards1[0]) {
+    for (const element of goal) {
+        if (element === cards1[0]) {
             cards1.shift()
-            continue;
-        }
-
-        if (goal[i] === cards2[0]) {
+        } else if (element === cards2[0]) {
             cards2.shift()
-            continue;
+        } else {
+            return 'No';
         }
-
-        count--;
-        break;
     }
-
-    if (count === goal.length) {
-        answer = "Yes"
-    }
-    return answer;
+    return 'Yes';
 }
 console.log(solution(["i", "drink"], ["want", "to"], ["i", "want", "to", "drink", "water"]))
 
