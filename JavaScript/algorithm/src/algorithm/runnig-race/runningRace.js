f27e43946ef19b5d1ff0566c5b2b228621d74ac5
akdl911215 <akdl911215@naver.com>
2023-04-09 23:49:08 +0900

    running race)

---
+++ JavaScript/algorithm/src/algorithm/runnig-race/runningRace.js
@@ -0,0 +1,9 @@
+function solution(players, callings) {
+    var answer = [];
+    return answer;
+}
+
+solution(["mumu", "soe", "poe", "kai", "mine"], ["kai", "kai", "mine", "mine"]);
+// ["mumu", "kai", "mine", "soe", "poe"]
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=javascript
