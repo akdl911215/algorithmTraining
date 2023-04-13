8b939d31e67f6f0b4341d766a35d4203c40b2c58
akdl911215 <akdl911215@naver.com>
2023-04-13 12:40:05 +0900

    algorithm array sum

--- JavaScript/algorithm/src/algorithm/arraySum/arraySum.js
+++ JavaScript/algorithm/src/algorithm/arraySum/arraySum.js
@@ -1,22 +1,31 @@
 function solution(sequence, k) {
-    var answer = [];
+    let answer = [];
 
     const pivot = k;
 
     const length = sequence.length;
+    console.log('length : ', length)
 
     for (let i = 0; i < length; ++i) {
+        // console.log('cehck',i)
         let sum = 0;
 
-        for (let j = 0; j < length; ++j) {
-            sum += sequence[i];
+        for (let j = i; j < length; ++j) {
+            console.log('2222 j : ', j)
+            sum = sum + sequence[i];
+            console.log('sum : ', sum)
+
+            if (pivot > sum) continue
 
             if (pivot === sum) {
+                answer.push(i)
+                answer.push(j)
                 break;
             }
         }
     }
 
+    console.log(answer)
     return answer;
 }
 
