3755a2538b8d4d1b8585564f59a90d3af016bf04
akdl911215 <akdl911215@naver.com>
2023-04-21 12:28:45 +0900

    array sum study

---
+++ JavaScript/algorithm/src/algorithm/arraySum/arraySum10.js
@@ -0,0 +1,85 @@
+function solution(sequence, pivot) {
+
+    const checkArr = [];
+    let ifCheck = true;
+
+    let sum = 0;
+    let start = 0;
+    let end = 0
+
+    let count = 0;
+
+    while (sequence.length !== count) {
+
+        const endArr = sequence[end];
+        sum += endArr;
+
+        if(start + 1 === sequence.length) {
+            console.log('ending : ', start + 1)
+            count = sequence.length
+            console.log('eeeee : ', sequence.length !== count)
+        }
+
+        if (sum < pivot) {
+            console.log('sum < pivot : ',sum < pivot, sum, pivot)
+            end += 1;
+            console.log('--------------------------------')
+        } else if (sum > pivot) {
+            console.log('sum > pivot : ', sum > pivot, sum, ':', pivot);
+            start += 1;
+            console.log('start : ', start)
+            end = start;
+            console.log('end : ', end)
+            sum = 0;
+            console.log('sum : ', sum)
+
+            count++;
+            console.log('--------------------------------')
+        } else if (sum === pivot) {
+
+            console.log('sum :::: ', sum)
+            console.log(`else if start: ${start}, end: ${end}`)
+            console.log('checkArr[2] : ' , checkArr[2])
+            // 같으면 값 할당
+            if (end - start < checkArr[2]) {
+                console.log('first if : ', end - start,'<' ,checkArr[2])
+
+                checkArr[0] = start;
+                checkArr[1] = end;
+                checkArr[2] = end - start;
+                console.log('checkArr : ', checkArr)
+            }
+
+            if (ifCheck) {
+                console.log('second if')
+                checkArr[0] = start;
+                checkArr[1] = end;
+                checkArr[2] = end - start;
+
+                ifCheck = false;
+                console.log('checkArr : ',checkArr,', ifCheck : ', ifCheck)
+            }
+
+            start += 1;
+            end = start;
+            sum = 0;
+            count++;
+            console.log('--------------------------------')
+        }
+
+    }
+
+    console.log([checkArr[0], checkArr[1]])
+    return [checkArr[0], checkArr[1]]
+}
+
+// solution([1, 2, 3, 4, 5], 7);
+// [2, 3]
+
+solution([1, 1, 1, 2, 3, 4, 5]	,5);
+// [6, 6]
+
+// solution([2, 2, 2, 2, 2], 6)
+// [0, 2]
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/178870
--- JavaScript/algorithm/src/algorithm/arraySum/test3.js
+++ JavaScript/algorithm/src/algorithm/arraySum/test3.js
@@ -5,8 +5,11 @@ console.log(!1)
 console.log(!2)
 console.log(!-1)
 
-// solution([1, 1, 1, 2, 3, 4, 5]	,5);
+// solution([1, 2, 3, 4, 5], 7);
+// [2, 3]
+
+solution([1, 1, 1, 2, 3, 4, 5]	,5);
 // [6, 6]
 
-solution([2, 2, 2, 2, 2], 6)
+// solution([2, 2, 2, 2, 2], 6)
 // [0, 2]
