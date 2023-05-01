c3b9574df538cedfd97e6ec8bd565b13139aa5c2
akdl911215 <akdl911215@naver.com>
2023-05-01 22:53:35 +0900

    interception system

---
+++ JavaScript/algorithm/src/algorithm/interceptionSystem/interceptionSystem.js
@@ -0,0 +1,15 @@
+function solution(targets) {
+    var answer = 0;
+
+
+    return answer;
+}
+
+solution([[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]]);
+// 3
+
+// [4,5] [4,8] [3,7] 5이상 8이하
+// [10,14] [11,13] [5,12] 6이상 13이하
+// [1,4] // 2이상 3이하
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181188
