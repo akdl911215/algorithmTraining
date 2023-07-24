f906f737f70c6b8d953308ef7449979dec6cc0b4
akdl911215 <akdl911215@naver.com>
2023-07-24 13:16:52 +0900

    programmers 예상대진표

--- JavaScript/algorithm/src/algorithm/예상대진표/예상대진표2.js
+++ JavaScript/algorithm/src/algorithm/예상대진표/예상대진표2.js
@@ -1,13 +1,12 @@
 function solution(n, a, b) {
   let divide = n / 2;
 
-  let result = 0;
-  for (let i = 0; i < divide; ++i) {
+  let result = Math.log2(n);
+  for (let i = 0; i < Math.log2(n); ++i) {
     if (a <= divide && b > divide) {
-      result = Math.log2(divide * 2);
       break;
     } else {
-      divide /= 2;
+      --result;
     }
   }
 
@@ -16,7 +15,7 @@ function solution(n, a, b) {
 console.log(solution(8, 4, 7));
 // 3
 
-console.log(solution(16, 4, 9));
+// console.log(solution(16, 1, 4));
 // 4
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/12985
