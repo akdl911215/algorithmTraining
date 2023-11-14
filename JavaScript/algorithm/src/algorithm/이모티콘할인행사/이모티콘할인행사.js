97ae02d68e8525e9d5ab95a64baec459311a842b
akdl911215 <akdl911215@naver.com>
2023-11-14 20:46:07 +0900

    이모티콘 할인행사

--- JavaScript/algorithm/src/algorithm/이모티콘할인행사/이모티콘할인행사.js
+++ JavaScript/algorithm/src/algorithm/이모티콘할인행사/이모티콘할인행사.js
@@ -2,6 +2,54 @@ function solution(users, emoticons) {
   console.log("users : ", users);
   let answer = [];
 
+  const userList = users.map((value) => {
+    let el = value[0];
+
+    if (20 > el && el >= 10) {
+      el = 20;
+    } else if (30 > el) {
+      el = 30;
+    } else if (40 > el) {
+      el = 40;
+    }
+
+    return [el, value[1]];
+  });
+  console.log("userList : ", userList);
+
+  const [emotionA, emotionB] = emoticons;
+
+  const promotionList = [
+    [0.4, 0.4],
+    [0.3, 0.4],
+    [0.2, 0.4],
+    [0.1, 0.4],
+    [0.4, 0.3],
+    [0.4, 0.2],
+    [0.4, 0.1],
+    [0.3, 0.3],
+    [0.2, 0.2],
+    [0.1, 0.1],
+  ];
+
+  let number = Infinity;
+  let count = 0;
+  for (let i = 0; i < promotionList.length; ++i) {
+    const [promotionA, promotionB] = promotionList[i];
+    const a = emotionA - emotionA * promotionA;
+    const b = emotionB - emotionB * promotionB;
+
+    const ab = a + b;
+    if (ab >= 10000 && number > ab) {
+      number = ab;
+    }
+  }
+  // 이모티콘 플러스 서비스 가입 수와 이모티콘 매출액을 배열로
+
+  emoticons.forEach((value) => {
+    //
+  });
+
   // 할인율은 10%, 20%, 30%, 40%
   // 최대한 1만원에서 좀만 넘어가게
   return answer;
