de22207e87e767e381355a3f4adfc375aba10f5a
akdl911215 <akdl911215@naver.com>
2023-11-14 12:08:14 +0900

    이모티콘 할인행사

---
+++ JavaScript/algorithm/src/algorithm/이모티콘할인행사/이모티콘할인행사.js
@@ -0,0 +1,37 @@
+function solution(users, emoticons) {
+  console.log("users : ", users);
+  let answer = [];
+
+  // 할인율은 10%, 20%, 30%, 40%
+  // 최대한 1만원에서 좀만 넘어가게
+  return answer;
+}
+
+console.log(
+  solution(
+    [
+      [40, 10000],
+      [25, 10000],
+    ],
+    [7000, 9000]
+  )
+);
+// [1, 5400]
+
+// console.log(
+//   solution(
+//     [
+//       [40, 2900],
+//       [23, 10000],
+//       [11, 5200],
+//       [5, 5900],
+//       [40, 3100],
+//       [27, 9200],
+//       [32, 6900],
+//     ],
+//     [1300, 1500, 1600, 4900]
+//   )
+// );
+// [4, 13860]
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/150368
