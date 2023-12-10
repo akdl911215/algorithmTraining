74725efb9b2a742907f83e1df20d5764e1df363b
akdl911215 <akdl911215@naver.com>
2023-12-10 23:20:08 +0900

    두 큐 합 같게 만들기

--- JavaScript/algorithm/src/algorithm/두큐합같게만들기/두큐합같게만들기.js
+++ JavaScript/algorithm/src/algorithm/두큐합같게만들기/두큐합같게만들기.js
@@ -1,18 +1,32 @@
 function solution(queue1, queue2) {
-  let answer = -2;
+  let answer = 0;
 
-  const pivot =
-    [...queue1, ...queue2].reduce(
+  while (true) {
+    const q1 = queue1.reduce(
       (accumulator, currentValue) => accumulator + currentValue,
       0
-    ) / 2;
-  console.log(pivot);
+    );
+    const q2 = queue2.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    if (!q1 || !q2) return -1;
 
-  return answer;
+    if (q1 === q2) {
+      return answer;
+    } else if (q1 > q2) {
+      queue2.push(queue1.shift());
+    } else if (q2 > q1) {
+      queue1.push(queue2.shift());
+    }
+
+    answer++;
+  }
 }
 
+// 14 16
 console.log(solution([3, 2, 7, 2], [4, 6, 5, 1])); // 2
-// console.log([1, 2, 1, 2], [1, 10, 1, 2]); // 7
-// console.log([1, 1], [1, 5]); // -1
+console.log(solution([1, 2, 1, 2], [1, 10, 1, 2])); // 7
+console.log(solution([1, 1], [1, 5])); // -1
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/118667
