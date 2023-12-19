c42143cbc85feae1934647548416e5d5d04273e7
akdl911215 <akdl911215@naver.com>
2023-12-19 23:16:25 +0900

    al study

---
+++ JavaScript/algorithm/src/algorithm/k진수에서소수개수구하기/test.js
@@ -0,0 +1,13 @@
+const a = 128;
+// console.log(parseInt(a, 2));
+
+console.log(Number(a.toString(2)));
+// console.log(a.toString(3));
+
+const b = "128";
+console.log(parseInt(b, 2));
+
+const c = 9;
+console.log(Math.sqrt(c));
+
+console.log(!!0);
