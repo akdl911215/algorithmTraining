aff53f84ce16b233591470cb8cea052e987b8739
akdl911215 <akdl911215@naver.com>
2023-12-27 10:06:09 +0900

    롤케이크자르기

--- JavaScript/algorithm/src/algorithm/롤케이크자르기/롤케이크자르기7.js
+++ JavaScript/algorithm/src/algorithm/롤케이크자르기/롤케이크자르기7.js
@@ -2,25 +2,28 @@ function solution(topping) {
   let answer = 0;
   const cakeTable = new Map();
   const sendingSet = new Set(topping);
-  console.log("sendingSet : ", sendingSet);
 
   for (let i = 0; i < topping.length; ++i)
     cakeTable.set(topping[i], (cakeTable.get(topping[i]) || 0) + 1);
-
-  console.log("cakeTable : ", cakeTable);
 
   const receivingSet = new Set();
   topping.forEach((tp) => {
     if (cakeTable.get(tp) >= 1) {
       cakeTable.set(tp, cakeTable.get(tp) - 1);
+      receivingSet.add(tp);
     }
 
-    if (cakeTable.get(tp) === 0) {
+    if (!cakeTable.get(tp)) {
       cakeTable.delete(tp);
+      sendingSet.delete(tp);
     }
+
+    if (receivingSet.size === sendingSet.size) answer++;
   });
 
   return answer;
 }
 console.log(solution([1, 2, 1, 3, 1, 4, 1, 2]));
 // 2
+console.log(solution([1, 2, 3, 1, 4]));
+// 0
