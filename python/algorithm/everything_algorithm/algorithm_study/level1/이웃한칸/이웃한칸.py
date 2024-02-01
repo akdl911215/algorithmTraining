cf937d91d3e5752b1a8daa698784b03b6ceeeb0d
akdl911215 <akdl911215@naver.com>
2024-02-01 23:55:57 +0900

    이웃한 칸

---
+++ python/algorithm/everything_algorithm/algorithm_study/level1/이웃한칸/이웃한칸.py
@@ -0,0 +1,34 @@
+def solution(board, h, w):
+    answer = 0
+    select_board = board[h][w]
+
+    # 높이가 맨 위 일 경우를 제외
+    if h > 0:
+        if select_board == board[h - 1][w]:
+            answer += 1
+
+    # 높이가 맨 아래 일 경우를 제외
+    if len(board) - 1 > h:
+        if select_board == board[h + 1][w]:
+            answer += 1
+
+    # 넓이의 위치가 맨 왼쪽 일 경우 제외
+    if w > 0:
+        if select_board == board[h][w - 1]:
+            answer += 1
+
+    # 넓이의 위치가 맨 오른쪽 일 경우 경우 제외
+    if len(board[0]) - 1 > w:
+        if select_board == board[h][w + 1]:
+            answer += 1
+
+    return answer
+
+
+print(solution([["blue", "red", "orange", "red"], ["red", "red", "blue", "orange"], ["blue", "orange", "red", "red"], ["orange", "orange", "red", "blue"]], 1, 1))
+# 2
+
+print(solution([["yellow", "green", "blue"], ["blue", "green", "yellow"], ["yellow", "blue", "blue"]], 0, 1))
+# 1
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/250125
