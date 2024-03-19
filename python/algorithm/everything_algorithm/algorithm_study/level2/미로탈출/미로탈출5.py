7398c5221eb078c40e42d291db183c606f95368a
akdl911215 <akdl911215@naver.com>
2024-03-19 11:56:41 +0900

    미로탈출

--- python/algorithm/everything_algorithm/algorithm_study/level2/미로탈출/미로탈출5.py
+++ python/algorithm/everything_algorithm/algorithm_study/level2/미로탈출/미로탈출5.py
@@ -4,10 +4,10 @@ from collections import deque
 def bfs(maps, N, M, start, end, count, coordinates_count_list):
     check_coordinates_list = [[float('inf') for _ in range(M)] for _ in range(N)]
     check_coordinates_list[start[0]][start[1]] = 0
+    pivot = 0
 
     queue = deque([[start[0], start[1], count]])
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)] # 상하좌우
-
 
     while queue:
         x, y, c = queue.popleft()
@@ -16,20 +16,14 @@ def bfs(maps, N, M, start, end, count, coordinates_count_list):
         for i in range(4):
             dx, dy = directions[i]
 
-            print('coordinates_count_list[nx + dx][ny + dy] : ', coordinates_count_list[nx + dx][ny + dy])
-            print('c : ', c)
-            while 0 <= nx + dx < N and 0 <= ny + dy < M and coordinates_count_list[nx + dx][ny + dy] >= c and maps[nx + dx][ny + dy] != 'X':
-                nx += dx
-                ny += dy
+            while 0 <= nx + dx < N and 0 <= ny + dy < M and maps[nx][ny] != 'X' and coordinates_count_list[nx][ny] == 0:
+                nx += x
+                ny += y
                 c += 1
-                coordinates_count_list[nx][ny] = c
 
-
-            if 0 <= nx < N and 0 <= ny < M and check_coordinates_list[nx][ny] > c and coordinates_count_list[nx][ny] >= c:
+            if 0 <= nx < N and 0 <= ny < M and check_coordinates_list[nx][ny] > pivot:
+                pivot += 1
                 queue.append([nx, ny, c])
-
-                if nx == end[0] and ny == end[1]:
-                    return [nx, ny, c]
 
     return [-1]
 
@@ -53,7 +47,7 @@ def solution(maps):
             if maps[i][j] == 'E':
                 exit[0], exit[1] = i, j
 
-    coordinates_count_list = [[float('inf') for _ in range(M)] for _ in range(N)]
+    coordinates_count_list = [[0 for _ in range(M)] for _ in range(N)]
     coordinates_count_list[start[0]][start[1]] = 0
     end = lever
     lever_position_information = bfs(maps, N, M, start, end, 0, coordinates_count_list)
