949414f1c89cc334e4ebb7d6556ef91fddb62fae
akdl911215 <akdl911215@naver.com>
2024-04-12 17:51:17 +0900

    N으로 표현

--- java2/src/N으로표현/N으로표현.java
+++ java2/src/N으로표현/N으로표현.java
@@ -1,42 +1,11 @@
 package N으로표현;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class N으로표현 {
 
-    private static int repeatN(int N, int count) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < count; i++) {
-            sb.append(N);
-        }
-        return Integer.parseInt(sb.toString());
-    }
+
     public static int solution(int N, int number) {
-        if (N == number) return 1;
 
-        List<Set<Integer>> dp = new ArrayList<>();
-        for (int i = 0; i <= 8; i++) dp.add(new HashSet<>());
-
-        for (int i = 1; i <= 8; i++) {
-            int base = repeatN(N, i);
-            dp.get(i).add(base);
-
-            for (int j = 1; j < i; j++) {
-                for (int a : dp.get(j)) {
-                    int ij = i - j;
-                    for (int b : dp.get(ij)) {
-                        dp.get(i).add(a + b);
-                        dp.get(i).add(a - b);
-                        dp.get(i).add(a * b);
-                        if (b != 0) dp.get(i).add(a / b);
-                    }
-                }
-            }
-            if (dp.get(i).contains(number)) return i;
-        }
 
         return -1;
     }
@@ -53,3 +22,5 @@ public class N으로표현 {
         // 3
     }
 }
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42895
