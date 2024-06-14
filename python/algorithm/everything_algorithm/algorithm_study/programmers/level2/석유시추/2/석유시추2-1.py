d3df02f82c7a0a8168ac5efb3f0b7d0bc2817802
akdl911215 <akdl911215@naver.com>
2024-06-15 08:56:30 +0900

    석유시추

--- python/algorithm/everything_algorithm/algorithm_study/programmers/level2/석유시추/2/석유시추2-1.py
+++ python/algorithm/everything_algorithm/algorithm_study/programmers/level2/석유시추/2/석유시추2-1.py
@@ -1,9 +1,11 @@
 from collections import deque
 
-def bfs(graph, start):
-    N, M = start
-    queue = deque([[N, M]])
+def bfs(graph, visited, start):
+    end = start[1]
+    queue = deque([start])
     oil_size = 1
+
+    # Y값에 대해서 ...
 
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     while queue:
@@ -12,33 +14,30 @@ def bfs(graph, start):
         for x, y in directions:
             nx, ny = q_n + x, q_m + y
 
-            if 0 <= nx <= len(graph) and 0 <= ny <= len(graph[0]) and graph[nx][ny] == 1:
+            if 0 <= nx <= len(graph) - 1 and 0 <= ny <= len(graph[0]) - 1 and graph[nx][ny] == 1 and not visited[nx][ny]:
                 oil_size += 1
+                visited[nx][ny] = True
                 queue.append([nx, ny])
+                end = ny
 
-    return oil_size
+    return [start[1], end, oil_size]
 
 def solution(land):
 
     N = len(land)
     M = len(land[0])
-
-    visited = [[False for _ in range(M)] for _ in range(N)]
-    print('visited : ', visited)
     oil_size_list = [0 for _ in range(M)]
-    print('oil_size_list : ', oil_size_list)
+    visited = [[False for _ in range(M)] for _ in range(N)]
 
     for i in range(N):
         for j in range(M):
-            if land[i][j] == 1:
-                oil_size = bfs(land, [i, j])
+            if land[i][j] == 1 and not visited[i][j]:
                 visited[i][j] = True
-                oil_size_list[j] += oil_size
+                start, end, oil_size = bfs(land, visited,[i, j])
+                for oil_pivot in range(start, end + 1):
+                    oil_size_list[oil_pivot] += oil_size
 
-    revl = max(oil_size_list)
-    print('revl : ' + revl)
-
-    return revl
+    return max(oil_size_list)
 
 print(
     solution(
@@ -53,19 +52,19 @@ print(
 )
 # 9
 
-# print(
-#     solution(
-#         [
-#             [1, 0, 1, 0, 1, 1],
-#             [1, 0, 1, 0, 0, 0],
-#             [1, 0, 1, 0, 0, 1],
-#             [1, 0, 0, 1, 0, 0],
-#             [1, 0, 0, 1, 0, 1],
-#             [1, 0, 0, 0, 0, 0],
-#             [1, 1, 1, 1, 1, 1]
-#         ]
-#     )
-# )
+print(
+    solution(
+        [
+            [1, 0, 1, 0, 1, 1],
+            [1, 0, 1, 0, 0, 0],
+            [1, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0, 0],
+            [1, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1]
+        ]
+    )
+)
 # 16
 
 #https://school.programmers.co.kr/learn/courses/30/lessons/250136?language=python3
