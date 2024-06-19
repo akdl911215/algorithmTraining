a47b19374391dd08910e40d92c154e960624016d
akdl911215 <akdl911215@naver.com>
2024-06-19 11:02:24 +0900

    javascript Oil drilling

--- JavaScript/algorithm/src/algorithm/석유시추/석유시추.js
+++ JavaScript/algorithm/src/algorithm/석유시추/석유시추.js
@@ -8,11 +8,11 @@ class Queue {
     }
 
     dequeue() {
-        if (this.items.length === 0) {
-            return "Queue is empty";
-        }
-
         return this.items.shift();
+    }
+
+    size() {
+        return this.items.length;
     }
 }
 
@@ -24,12 +24,11 @@ const bfs = (graph, visited, start) => {
 
     const queue = new Queue();
     queue.enqueue([N, M]);
-    console.log('queue : ', queue);
 
     let oilSize = 1;
 
     const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-    while (!!queue) {
+    while (queue.size() !== 0) {
         const [qn, qm] = queue.dequeue();
 
         for (let i = 0; i < 4; ++i) {
@@ -37,16 +36,16 @@ const bfs = (graph, visited, start) => {
             const nx = x + qn;
             const ny = y + qm;
 
-            if (0 <= nx && nx < graph.length - 1 && 0 <= ny && nx < graph[0].length - 1 && graph[nx][ny] === 1 && !visited[nx][ny]) {
+            if (0 <= nx && nx < graph.length && 0 <= ny && nx < graph[0].length  && graph[nx][ny] === 1 && !visited[nx][ny]) {
                 queue.enqueue([nx, ny]);
                 visited[nx][ny] = true;
                 oilSize++;
 
-                if (minY < ny) {
+                if (ny < minY) {
                     minY = ny;
                 }
 
-                if (ny < maxY) {
+                if (maxY < ny) {
                     maxY = ny;
                 }
             }
@@ -57,19 +56,19 @@ const bfs = (graph, visited, start) => {
 }
 
 const solution = (land) => {
-    let answer = 0;
 
     const N = land.length;
     const M = land[0].length;
     const visited = Array.from({length: N}, ()=> Array(M).fill(false));
-    const oilSizeList = Array.from({length: N}, () => 0);
+    const oilSizeList = Array.from({length: M}, () => 0);
 
     land.forEach((_, i) => {
         land[0].forEach((_, j) => {
-            if (land[i][j] === 1 && !visited[i][j] && !visited[i][j]) {
+            if (land[i][j] === 1 && !visited[i][j] ) {
+
                 const [start, end, oilSize] = bfs(land, visited, [i, j]);
 
-                for (let i = start; i < end + 1; ++i) {
+                for (let i = start; i <= end; ++i) {
                     oilSizeList[i] += oilSize;
                 }
             }
@@ -81,32 +80,22 @@ const solution = (land) => {
     return Math.max(...oilSizeList);
 }
 
+// cos
+// # 9
+
 console.log(
     solution(
         [
-            [0, 0, 0, 1, 1, 1, 0, 0],
-            [0, 0, 0, 0, 1, 1, 0, 0],
-            [1, 1, 0, 0, 0, 1, 1, 0],
-            [1, 1, 1, 0, 0, 0, 0, 0],
-            [1, 1, 1, 0, 0, 0, 1, 1]
+            [1, 0, 1, 0, 1, 1],
+            [1, 0, 1, 0, 0, 0],
+            [1, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0, 0],
+            [1, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1]
         ]
     )
 )
-// # 9
-
-// print(
-//     solution(
-//         [
-//             [1, 0, 1, 0, 1, 1],
-//             [1, 0, 1, 0, 0, 0],
-//             [1, 0, 1, 0, 0, 1],
-//             [1, 0, 0, 1, 0, 0],
-//             [1, 0, 0, 1, 0, 1],
-//             [1, 0, 0, 0, 0, 0],
-//             [1, 1, 1, 1, 1, 1]
-//         ]
-//     )
-// )
 // # 16
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/250136?language=python3
