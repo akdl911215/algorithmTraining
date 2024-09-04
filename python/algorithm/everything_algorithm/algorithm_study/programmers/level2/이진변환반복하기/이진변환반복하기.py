40a8c329136c7b590153fba50a31d29b4956c839
akdl911215 <akdl911215@naver.com>
2024-09-05 08:57:14 +0900

    이진변환하기

--- python/algorithm/everything_algorithm/algorithm_study/programmers/level2/이진변환반복하기/이진변환반복하기.py
+++ python/algorithm/everything_algorithm/algorithm_study/programmers/level2/이진변환반복하기/이진변환반복하기.py
@@ -1,4 +1,5 @@
 def solution(s):
+    zero_remove_count = 0
     count = 0
 
     while s != '1':
@@ -8,20 +9,21 @@ def solution(s):
             if number == '1':
                 st += '1'
             else:
-                count += 1
+                zero_remove_count += 1
 
+        count += 1
         decimal = len(st)
         s = bin(decimal)[2:]
 
-    return [s, count]
+    return [count, zero_remove_count]
 
 print(solution("110010101001"))
 # [3,8]
 
-# print(solution("01110"))
+print(solution("01110"))
 # [3,3]
 
-# print(solution("1111111"))
+print(solution("1111111"))
 # [4,1]
 
 # https://school.programmers.co.kr/learn/courses/30/lessons/70129
