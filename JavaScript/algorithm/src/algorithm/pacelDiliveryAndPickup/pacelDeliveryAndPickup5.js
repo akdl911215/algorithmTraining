e77c260272d6555f61ae0b35f915ed814ce34f76
akdl911215 <akdl911215@naver.com>
2024-11-18 10:57:59 +0900

    pacel delivery and pickup alogorithm

---
+++ JavaScript/algorithm/src/algorithm/pacelDiliveryAndPickup/pacelDeliveryAndPickup5.js
@@ -0,0 +1,54 @@
+function solution(cap, n, deliveries, pickups) {
+  let answer = 0;
+
+  const deliveriesStack = [];
+  const pickupsStack = [];
+
+  for (let i = 0; i < deliveries.length; ++i) {
+    const delivery = deliveries[i];
+    const pickup = pickups[i];
+    const location = i + 1;
+
+    if (delivery !== 0) {
+      deliveriesStack.push([location, delivery]);
+    }
+
+    if (pickup !== 0) {
+      pickupsStack.push([location, pickup]);
+    }
+  }
+
+  while (deliveriesStack.length > 0 && pickupsStack.length > 0) {
+    let maxDistance = 0;
+    // const [pickupLocation, pickupCount] = pickupsStack[pickupsStack.length - 1];
+
+    // truckCapCount <= cap
+    let truckDeliveryCapCount = 0;
+    // const truckPickupCapCount = 0;
+
+    while (deliveriesStack.length > 0 && truckDeliveryCapCount < cap) {
+      const [deliveryLocation, deliveryCount] =
+        deliveriesStack[deliveriesStack.length - 1];
+
+      const availableCapacity = cap - truckDeliveryCapCount;
+      if (deliveryCount <= availableCapacity) {
+        truckDeliveryCapCount += deliveryCount;
+        deliveriesStack.pop();
+      } else {
+        truckDeliveryCapCount += availableCapacity;
+        deliveriesStack[deliveriesStack.length - 1][1] -= availableCapacity;
+      }
+
+      maxDistance = Math.max(maxDistance, deliveryLocation);
+    }
+  }
+
+  return answer;
+}
+console.log(solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0]));
+// 16
+
+// console.log(solution(2, 7, [1, 0, 2, 0, 1, 0, 2], [0, 2, 0, 1, 0, 2, 0]));
+// 30
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/150369
