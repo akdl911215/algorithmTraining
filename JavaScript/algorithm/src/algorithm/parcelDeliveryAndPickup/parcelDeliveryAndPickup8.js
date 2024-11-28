07021bd3cf16bad24d2d3ac555e8d18e34e6cf0d
akdl911215 <akdl911215@naver.com>
2024-11-28 10:17:25 +0900

    parcel delivery and pickup algorithm

--- JavaScript/algorithm/src/algorithm/parcelDeliveryAndPickup/parcelDeliveryAndPickup8.js
+++ JavaScript/algorithm/src/algorithm/parcelDeliveryAndPickup/parcelDeliveryAndPickup8.js
@@ -43,3 +43,8 @@ function solution(cap, n, deliveries, pickups) {
 
   return answer;
 }
+console.log(solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0]));
+// 16
+
+console.log(solution(2, 7, [1, 0, 2, 0, 1, 0, 2], [0, 2, 0, 1, 0, 2, 0]));
+// 30
