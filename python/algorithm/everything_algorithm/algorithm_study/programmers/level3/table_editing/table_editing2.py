de9b383016ee2fa7f44b1c1359575739ecdef76f
akdl911215 <akdl911215@naver.com>
2025-01-05 16:20:16 +0900

    table editing

--- python/algorithm/everything_algorithm/algorithm_study/programmers/level3/table_editing/table_editing2.py
+++ python/algorithm/everything_algorithm/algorithm_study/programmers/level3/table_editing/table_editing2.py
@@ -9,22 +9,22 @@ def solution(n, k, cmd):
             move = int(m)
 
             while move > 0:
-                current_row += 1
-
-                if table[current_row] == 'O':
-                    move -= 1
-
-        if command.startswith('D'):
-            down, m = command.split()
-            move = int(m)
-
-            while move > 0:
                 current_row -= 1
 
                 if table[current_row] == 'O':
                     move -= 1
 
-        if command.startswith('C'):
+        elif command.startswith('D'):
+            down, m = command.split()
+            move = int(m)
+
+            while move > 0:
+                current_row += 1
+
+                if table[current_row] == 'O':
+                    move -= 1
+
+        elif command.startswith('C'):
             deleted_stack.append(current_row)
             table[current_row] = 'X'
 
@@ -33,7 +33,7 @@ def solution(n, k, cmd):
             else:
                 current_row += 1
 
-        if command .startswith('Z'):
+        elif command .startswith('Z'):
             last_row_index = deleted_stack.pop()
             table[last_row_index] = 'O'
 
@@ -42,7 +42,7 @@ def solution(n, k, cmd):
 print(solution(8,2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]))
 # "OOOOXOOO"
 
-# print(solution(8,2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]))
+print(solution(8,2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]))
 # "OOXOXOOO"
 
 # https://school.programmers.co.kr/learn/courses/30/lessons/81303?language=python3
---
+++ python/algorithm/everything_algorithm/algorithm_study/programmers/level3/table_editing/table_editing4.py
@@ -0,0 +1,59 @@
+def solution(n, k, cmd):
+    answer = ['0'] * n
+    current_row = k
+    table = {i: [i - 1, i + 1] for i in range(n)}
+    table[0][0], table[n - 1][1] = None, None
+    deleted_stack = []
+
+    print(table)
+
+    for c in cmd:
+        if c.startswith('U'):
+            up, X = c.split()
+            move = int(X)
+
+            while move > 0:
+                move -= 1
+                current_row = table[current_row][1]
+            answer[current_row] = 'X'
+
+        elif c.startswith('D'):
+            down, X = c.split()
+            move = int(X)
+
+            while move > 0:
+                move -= 1
+                current_row = table[current_row][0]
+            answer[current_row] = 'X'
+
+        elif c.startswith('C'):
+            prev, next = table[current_row]
+            deleted_stack.append([prev, current_row, next])
+
+            if prev == None:
+                current_row = table[current_row][1]
+                table[current_row][0] = None
+            elif next == None:
+                current_row = table[current_row][0]
+                table[current_row][1] = None
+            else:
+                current_row = table[current_row][1]
+                table_prev = table[prev][1]
+                table[prev][1] = table[next][0]
+                table[next][0] = table_prev
+
+        else: # Z
+            [prev, current, next] = deleted_stack.pop()
+
+
+
+    return ''.join(answer)
+
+print(solution(8,2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]))
+# "OOOOXOOO"
+
+# print(solution(8,2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]))
+# "OOXOXOOO"
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/81303?language=python3
+# https://kjhoon0330.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%ED%91%9C-%ED%8E%B8%EC%A7%91-Python
