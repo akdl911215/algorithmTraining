30bfe9a1874301fe5b424cc466b10c7795995948
akdl911215 <akdl911215@naver.com>
2025-03-21 16:33:24 +0900

    merge sort

---
+++ JavaScript/algorithm/src/algorithm/merge-sort/mergeSort.js
@@ -0,0 +1,31 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  const a = mergeSort(left);
+  const b = mergeSort(right)
+  return merge(a, b);
+}
+
+function merge(left, right) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+const myArray = [6, 5, 12, 10, 9, 1];
+console.log(mergeSort(myArray)); // Output: [1, 5, 6, 9, 10, 12]
